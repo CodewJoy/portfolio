b52b68c2203d97907dd9c5d2c9795ba4bc978905
conditCat <yichen.hung6699@gmail.com>
2025-09-20 21:32:17 +0800

    modify for vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/",
   server: {
     port: 3000,
   },
-  base: "/portfolio/",
 });
